72b2600fcada91aa59be7ef9ced5f0f0d427cef8
TebaultTechnologyGroup <mark.tebault@tebaulttechnologygroup.com>
2026-01-28 15:43:10 -0500

    removing lamda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -7,13 +7,13 @@ import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 const backend = defineBackend({
   auth,
   data,
-  generateDocs, // This name must match the schema reference
+ // generateDocs, // This name must match the schema reference
 });
 
 // Grant the Lambda function permission to invoke Bedrock
-backend.generateDocs.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    actions: ['bedrock:InvokeModel'],
-    resources: ['*'],
-  })
-);
+// backend.generateDocs.resources.lambda.addToRolePolicy(
+//   new PolicyStatement({
+//     actions: ['bedrock:InvokeModel'],
+//     resources: ['*'],
+//   })
+// );
