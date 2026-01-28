ad611886628028343d7f5f2e7f95498f50f3380f
TebaultTechnologyGroup <mark.tebault@tebaulttechnologygroup.com>
2026-01-28 15:10:17 -0500

    changed folder name from -prd to -docs

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { generateDocs } from './functions/generate-prd/resource';
+import { generateDocs } from './functions/generate-docs/resource';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 const backend = defineBackend({
